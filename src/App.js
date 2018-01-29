4447a76ad40ba732d4c495b9b633be837469f003
codeomen <damioncms@gmail.com>
2018-01-29 09:16:38 -0600

    Fixed handler function declaration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchString: '',
+      resultsList: []
+    };
+    this.handleSearch = this.handleSearch.bind(this);
+  };
+  
+  handleSearch(e) {
+    this.setState({searchString: e.target.value});
+    fetch('./PricingData.json')
+      .then(results => JSON.parse(results))
+      .then(dataList => {this.setState({resultsList: dataList.filter.exec(`/${e.target.value}/`)})
+      })
+    console.log(this.state.resultsList);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            <input name="search" type="search" autoFocus="true" onChange={this.handleSearch} />
+          </h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.

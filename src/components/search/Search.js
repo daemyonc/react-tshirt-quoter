be0fe12c1c6158c9af1dac8a9fedc9379473469b
Daemyon <damioncms@gmail.com>
2021-06-15 21:56:10 -0500

    Move to individual component folders
    Add .eslintrc
    
    Change to components

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -4,9 +4,7 @@ class Search extends Component {
 
   render() {
     return (
-      <label>
         <input id="search" className="search" type="search" autoFocus="true" onChange={this.props.onChange} placeholder="Search Product Ids" />
-      </label>
       );
   }
 }
